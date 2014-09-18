21160fb3ccb6a31f85d58d84e49f97613a724bc7
Jernej Virag <jernej@virag.si>
2014-09-19 01:17:48 +0200

    Fixed robots.txxt

--- estate_ads/urls.py
+++ estate_ads/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+from django.http import HttpResponse
 import views
 
 urlpatterns = patterns('',
-    url(r'', views.index_view)
+    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
+    url(r'', views.index_view),
 )
